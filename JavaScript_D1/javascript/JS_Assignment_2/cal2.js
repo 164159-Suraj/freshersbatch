fe3a6c66b2ef57b51bc4180da47251d93bf42d95
164159-Suraj <44641142+164159-Suraj@users.noreply.github.com>
2019-02-14 16:36:49 +0530

    Add files via upload

---
+++ JavaScript_D1/javascript/JS_Assignment_2/cal2.js
@@ -0,0 +1,26 @@
+function calculate()
+    {
+  var c=parseInt(prompt("Choose one value from the menu As for your choice"
+       switch(c) {
+          
+          case 1:
+             return(a+b);
+             break;
+          case 2:
+             return(a-b);
+             break;
+          case 3:
+             return(a+b);
+             break;
+          case 1:
+             return(a+b);
+             break;
+          case 1:
+             return(a+b);
+             break;
+          case 1:
+             return(a+b);
+             break;
+          case 1:
+             return(a+b);
+             break;
